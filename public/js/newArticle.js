b6050e432dd47fcb52d3f10802aaed57f2d0b92e
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-09-10 00:42:01 -0400

    files updates and article seeds

--- public/js/newArticle.js
+++ public/js/newArticle.js
@@ -1,36 +1,36 @@
 const newArticleFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.getElementById('new-Article-Title').value.trim();
-    const content = document.getElementById('new-Article-Content').value.trim();
+  const title = document.getElementById('new-Article-Title').value.trim();
+  const content = document.getElementById('new-Article-Content').value.trim();
 
-    if (title && content) {
-        const response = await fetch('/api/articles/', {
-            method: 'POST',
-            body: JSON.stringify({ title, content }),
-            headers: { "Content-Type": "application/json" },
-        });
+  if (title && content) {
+    const response = await fetch('/api/articles/', {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
     }
+  }
 };
 
 const cancelButtonHandler = async () => {
-    document.location.replace('/dashboard');
+  document.location.replace('/dashboard');
 }
 
 document
-    .querySelector('.newArticleForm')
-    .addEventListener('submit', newArticleFormHandler);
+  .querySelector('.newArticleForm')
+  .addEventListener('submit', newArticleFormHandler);
 
 // document
 // .querySelector('#btn-cancel')
 // .addEventListener('reset', cancelButtonHandler);
 
 document
-    .querySelector('.newArticleForm')
-    .addEventListener('reset', cancelButtonHandler);
+  .querySelector('.newArticleForm')
+  .addEventListener('reset', cancelButtonHandler);
